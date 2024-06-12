d6ed5d5b755c2c0f9162d60bef1e50e944a0fb76
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-06-13 02:42:10 +0300

    chore: Add variable declarations and assignments to Program.cs

--- Section 1/HelloWorld2/HelloWorld2/Program.cs
+++ Section 1/HelloWorld2/HelloWorld2/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Lirone!");
+
+// Declare a Variable
+string myFriendsName;
+
+// Assign a Value to the Variable
+myFriendsName = "Ohad";
+
+// Print the Value of the Variable
+Console.WriteLine(myFriendsName);
+
+// Overwrite the Value of the Variable
+myFriendsName = "Daniel";
+
+// Print the New Value of the Variable
+Console.WriteLine(myFriendsName);
+
+// Declare and Assign a Value to a Variable
+string myName = "Lirone";
+
+// Print the Value of the Variable
+Console.WriteLine(myName);
+
+// Console.ReadKey();
---
+++ Section 1/MyAdditionCalculator/MyAdditionCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Print "Hello, World!" to the console in C#.
+Console.WriteLine("Hello, World!");
+
+// Takes the user input and stores it in a variable.
+// Variable name: userInput, Variable type: string
+// Srting is a data type that represents text.
+Console.WriteLine("Enter a value: ");
+string? userInput = Console.ReadLine();
+
+// Prints the user input to the console.
+Console.WriteLine("You entered " + userInput);
+Console.ReadKey();
